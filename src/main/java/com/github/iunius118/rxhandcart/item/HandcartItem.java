d982ae2be97928947475be621b1d3f3b6d73db5f
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-04-29 17:25:10 +0900

    Add handcart item class

---
+++ src/main/java/com/github/iunius118/rxhandcart/item/HandcartItem.java
@@ -0,0 +1,9 @@
+package com.github.iunius118.rxhandcart.item;
+
+import net.minecraft.item.Item;
+
+public class HandcartItem extends Item {
+    public HandcartItem(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/github/iunius118/rxhandcart/item/ModItems.java
@@ -0,0 +1,10 @@
+package com.github.iunius118.rxhandcart.item;
+
+import com.github.iunius118.rxhandcart.RxHandcart;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(RxHandcart.MOD_ID)
+public class ModItems {
+    public static final Item HANDCART = null;
+}
