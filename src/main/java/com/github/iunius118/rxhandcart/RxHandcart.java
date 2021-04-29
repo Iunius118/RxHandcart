8cc201183169fb3c921bdbc42631ab302594373f
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-04-29 17:30:11 +0900

    Add registry of item and data provider

--- src/main/java/com/github/iunius118/rxhandcart/RxHandcart.java
+++ src/main/java/com/github/iunius118/rxhandcart/RxHandcart.java
@@ -1,37 +1,34 @@
 package com.github.iunius118.rxhandcart;
 
-import net.minecraft.block.Block;
+import com.github.iunius118.rxhandcart.data.ModItemModelProvider;
+import com.github.iunius118.rxhandcart.data.ModLanguageProviders;
+import com.github.iunius118.rxhandcart.item.HandcartItem;
 import net.minecraft.block.Blocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+@Mod(RxHandcart.MOD_ID)
+public class RxHandcart {
+    public static final String MOD_ID = "rxhandcart";
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("rxhandcart")
-public class RxHandcart
-{
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public RxHandcart() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
@@ -50,32 +47,34 @@ public class RxHandcart
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event) {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
+        public static void onItemRegistry(final RegistryEvent.Register<Item> itemRegistryEvent) {
+            itemRegistryEvent.getRegistry().registerAll(
+                    new HandcartItem(new Item.Properties().tab(ItemGroup.TAB_MISC)).setRegistryName("handcart")
+            );
+        }
+
+        @SubscribeEvent
+        public static void gatherData(final GatherDataEvent event) {
+            DataGenerator generator = event.getGenerator();
+
+            if (event.includeServer()) {
+
+            }
+
+            if (event.includeClient()) {
+                ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+                generator.addProvider(new ModItemModelProvider(generator, existingFileHelper));
+                ModLanguageProviders.addTo(generator);
+            }
         }
     }
 }
