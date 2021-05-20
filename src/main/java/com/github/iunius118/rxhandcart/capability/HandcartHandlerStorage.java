873eeceaecac5ee4832cb14c2f4f14b886aeb620
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-05-21 02:41:55 +0900

    Fix Handcart's NBT storage

--- src/main/java/com/github/iunius118/rxhandcart/capability/HandcartHandlerStorage.java
+++ src/main/java/com/github/iunius118/rxhandcart/capability/HandcartHandlerStorage.java
@@ -9,6 +9,9 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.ItemStackHandler;
 
 public class HandcartHandlerStorage implements Capability.IStorage<IHandcartHandler> {
+    public final static String KET_INV = "Inv";
+    public final static String KET_INV_SLOT = "Slot";
+
     @Override
     public INBT writeNBT(Capability<IHandcartHandler> capability, IHandcartHandler instance, Direction side) {
         ItemStackHandler itemHandler = instance.getItemHandler();
@@ -20,26 +23,36 @@ public class HandcartHandlerStorage implements Capability.IStorage<IHandcartHand
 
             if (!stack.isEmpty()) {
                 CompoundNBT itemTag = new CompoundNBT();
-                itemTag.putInt("Slot", i);
+                itemTag.putInt(KET_INV_SLOT, i);
                 stack.save(itemTag);
                 nbtTagList.add(itemTag);
             }
         }
 
-        return nbtTagList;
+        CompoundNBT tag = new CompoundNBT();
+        tag.put(KET_INV, nbtTagList);
+
+        return tag;
     }
 
     @Override
     public void readNBT(Capability<IHandcartHandler> capability, IHandcartHandler instance, Direction side, INBT base) {
-        ItemStackHandler itemHandler = instance.getItemHandler();
-        ListNBT tagList = (ListNBT) base;
+        if (!(base instanceof CompoundNBT)) return;
 
-        for (int i = 0; i < tagList.size(); i++) {
-            CompoundNBT itemTags = tagList.getCompound(i);
-            int j = itemTags.getInt("Slot");
+        CompoundNBT tag = (CompoundNBT) base;
 
-            if (j >= 0 && j < itemHandler.getSlots()) {
-                itemHandler.setStackInSlot(j, ItemStack.of(itemTags));
+        ListNBT tagList = (ListNBT) tag.get(KET_INV);
+
+        if (tagList != null) {
+            ItemStackHandler itemHandler = instance.getItemHandler();
+
+            for (int i = 0; i < tagList.size(); i++) {
+                CompoundNBT itemTags = tagList.getCompound(i);
+                int j = itemTags.getInt(KET_INV_SLOT);
+
+                if (j >= 0 && j < itemHandler.getSlots()) {
+                    itemHandler.setStackInSlot(j, ItemStack.of(itemTags));
+                }
             }
         }
     }
