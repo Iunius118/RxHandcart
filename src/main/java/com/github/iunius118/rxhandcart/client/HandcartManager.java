a9f74e6cbbbac8e6c70556b8cae60cc4f1008b06
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2022-06-27 21:52:16 +0900

    Add getTexture() to IHandcartModel

--- src/main/java/com/github/iunius118/rxhandcart/client/HandcartManager.java
+++ src/main/java/com/github/iunius118/rxhandcart/client/HandcartManager.java
@@ -8,7 +8,6 @@ import javax.annotation.Nullable;
 
 public class HandcartManager {
     private static final HandcartModel HANDCART_MODEL_1 = new HandcartModel();
-    private static final ResourceLocation HANDCART_LOCATION_1 = new ResourceLocation("textures/block/oak_planks.png");
 
     @Nullable
     public static IHandcartModel getHandcartModel(int type) {
@@ -22,7 +21,7 @@ public class HandcartManager {
     @Nullable
     public static ResourceLocation getHandcartTexture(int type) {
         if (type == 1) {
-            return HANDCART_LOCATION_1;
+            return HANDCART_MODEL_1.getTexture();
         }
 
         return null;
--- src/main/java/com/github/iunius118/rxhandcart/client/model/IHandcartModel.java
+++ src/main/java/com/github/iunius118/rxhandcart/client/model/IHandcartModel.java
@@ -2,7 +2,10 @@ package com.github.iunius118.rxhandcart.client.model;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.resources.ResourceLocation;
 
 public interface IHandcartModel {
     default void render(PoseStack matrixStack, VertexConsumer vertexBuilder, int lightmapCoord, int overlayColor, float red, float green, float blue, float alpha){}
+
+    ResourceLocation getTexture();
 }
